fce48a77d971f9499ff7c0a90f19d45a7f103681
Krzysztof_Tomkow <218514@student.pwr.edu.pl>
2019-07-30 19:40:58 +0200

    Another example

--- MySiteApi.ApiTests/UnitTest1.cs
+++ MySiteApi.ApiTests/UnitTest1.cs
@@ -1,20 +1,26 @@
 using FluentAssertions;
 using NUnit.Framework;
+using System.Collections.Generic;
 
 namespace Tests
 {
     [TestFixture]
     public class Tests
     {
+        private List<int> list = new List<int>();
+
         [SetUp]
         public void Setup()
         {
+            //list = new List<int>(); // uncomment to pass all the tests
         }
 
         [TestCaseSource(nameof(pairs))]
         [MaxTime(100)]
         public void Test1(int actual, int expected)
         {
+            list.Should().BeEmpty();
+            list.Add(actual + expected);
             actual.Should().Be(expected);
         }
 
